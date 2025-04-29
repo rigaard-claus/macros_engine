1342118875397080acad1116ac094fa8bd24dc5c
rigaa <rigaa@DESKTOP-KSUTF5U>
2025-04-29 19:01:16 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace MacrosEngine
+{
+    public partial class MacrosHands : Form
+    {
+        public MacrosHands()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
